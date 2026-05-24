b107076d1ff1702f22af2b071dff066286d19569
Yogesh <hajare.yogesh0@gmail.com>
2026-05-24 16:17:27 +0530

    update feature in script.js-1

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 let cart = [];
 
-function addToCart(product) {
-    cart.push(product);
+function addToCart(productName){
 
-    alert(product + " added to cart!");
+cart.push(productName);
 
-    console.log("Cart Items:", cart);
+document.getElementById("cart-count").innerText = cart.length;
+
+alert(productName + " added to cart!");
+
+console.log(cart);
+
+}
+
+function searchProducts(){
+
+let input =
+document.getElementById("search")
+.value
+.toLowerCase();
+
+let products =
+document.querySelectorAll(".product");
+
+products.forEach(product=>{
+
+let productName =
+product.querySelector("h3")
+.innerText
+.toLowerCase();
+
+if(productName.includes(input))
+{
+product.style.display="block";
+}
+else
+{
+product.style.display="none";
+}
+
+});
+
 }
